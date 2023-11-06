e2af5c43a061f1bfed50413d3dc37c9fe37e3c26
Anthony Zou <zouzeren@gmail.com>
2023-11-06 19:34:47 +0800

    Create newDeployment.js

---
+++ scripts/newDeployment.js
@@ -0,0 +1,53 @@
+const hre = require("hardhat");
+const fs = require("fs/promises");
+
+async function main() {
+  const Token = await hre.ethers.getContractFactory("Token");
+  const token = await Token.deploy("1000000");
+  await token.deployed();
+
+  const DutchAuction = await hre.ethers.getContractFactory("DutchAuction");
+  const openingTime = (await ethers.provider.getBlock("latest")).timestamp + 1;
+  const closingTime =
+    (await ethers.provider.getBlock("latest")).timestamp + 1200;
+  const initialPrice = 8000;
+  const finalPrice = 1000;
+  const tokenMaxAmount = 10000;
+  const [deployer] = await hre.ethers.getSigners(); // This will get the deployer's address
+  const wallet = deployer.address;
+
+  const da = await DutchAuction.deploy(
+    openingTime,
+    closingTime,
+    initialPrice,
+    finalPrice,
+    wallet,
+    token,
+    tokenMaxAmount
+  );
+
+  await da.deployed();
+  await writeDeploymentInfo(token, "token.json");
+  await writeDeploymentInfo(da, "da.json");
+  console.log(await da.startingPrice());
+  console.log(await da.discountRate());
+}
+
+async function writeDeploymentInfo(contract, filename = "") {
+  const data = {
+    network: hre.network.name,
+    contract: {
+      address: contract.address,
+      signerAddress: contract.signer.address,
+      abi: contract.interface.format(),
+    },
+  };
+
+  const content = JSON.stringify(data, null, 2);
+  await fs.writeFile(filename, content, { encoding: "utf-8" });
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
